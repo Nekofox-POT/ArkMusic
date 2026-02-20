90617c6025f821e34e563a83a5cd830c7440cc3d
nekofox POT <1352220638@qq.com>
2026-02-20 16:03:26 +0800

    4.0使用arkwrb重构，完成了任务栏。

---
+++ entry/src/main/resources/rawfile/webview_gui/taskbar/js/bound.js
@@ -0,0 +1,6 @@
+// 绑定池
+const taskbar = document.getElementById('taskbar');
+const target_show = document.getElementById('target_show');
+const taskbar_page = document.getElementById('taskbar_page');
+const taskbar_page_icon = document.getElementById('taskbar_page_icon');
+const taskbar_target = document.getElementById('taskbar_target');
