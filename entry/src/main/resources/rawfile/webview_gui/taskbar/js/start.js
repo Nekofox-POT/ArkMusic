a2d2cf5c00d9905063298a6beac880f13b59b066
nekofox POT <1352220638@qq.com>
2026-04-20 13:49:33 +0800

    文件界面制作

--- entry/src/main/resources/rawfile/webview_gui/taskbar/js/start.js
+++ entry/src/main/resources/rawfile/webview_gui/taskbar/js/start.js
@@ -7,7 +7,7 @@ function init() {
     // 颜色更新
     set_background_color()
     // 页面归位
-    taskbar_page_update(0)   // 临时修改
+    taskbar_page_update(2)   // 临时修改
     // 音量归0
     set_vol(0)
     // 进度条归位
