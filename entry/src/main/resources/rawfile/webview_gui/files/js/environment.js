fae710fb5866e24f22d0951c1231de7dcbc0b697
nekofox POT <1352220638@qq.com>
2026-04-22 01:34:04 +0800

    文件界面制作

--- entry/src/main/resources/rawfile/webview_gui/files/js/environment.js
+++ entry/src/main/resources/rawfile/webview_gui/files/js/environment.js
@@ -8,6 +8,17 @@ const choice_bar = document.querySelector(".choice_bar")
 const choice_bar_scroll = document.getElementById("choice_bar_scroll")
 const choice_bar_items = document.querySelectorAll(".choice_bar_item")
 
+// 框架组件映射
+const frame_map = {
+    '所有歌曲': document.getElementById("all_song_frame"),
+    '文件夹': document.getElementById("folder_frame"),
+    '播放列表': document.getElementById("playlist_frame"),
+    '我的喜欢': document.getElementById("favorite_frame"),
+    '专辑': document.getElementById("album_frame"),
+    '歌手': document.getElementById("artist_frame"),
+    '播放最多': document.getElementById("most_played_frame")
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 变量配置 //
 ////////////
--- entry/src/main/resources/rawfile/webview_gui/files/js/start.js
+++ entry/src/main/resources/rawfile/webview_gui/files/js/start.js
@@ -3,6 +3,15 @@
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function init() {
+    // 设置默认选中项的文字颜色
+    const activeItem = document.querySelector('.choice_bar_item.active')
+    if (activeItem) {
+        const p = activeItem.querySelector('p')
+        if (p) {
+            p.classList.add('font_active_color')
+            p.style.color = active_color
+        }
+    }
     // 颜色更新
     set_background_color()
 }
